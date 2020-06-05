c545db351f28bb31bf0fbc60ecdd76895c235620
Maarten ten Voorde <m.tenvoorde1998@gmail.com>
2020-06-05 12:53:43 +0200

    Added plot generation

--- Matlab files/SYSID/SubID.m
+++ Matlab files/SYSID/SubID.m
@@ -5,16 +5,18 @@ clear; clc; close all
 
 %% Load and initialise experiment data
 %load experiment data
-load('..\..\Data\blockResponse2.mat');
+load('..\..\Data\blockResponse5.mat');
 
 % init output
-y1 = t1.block;
-y2 = t2.block;
+y1 = t1.long;
+y2 = t2.long;
 y = [y1; y2];
 
 % init input
 u = [u1; u2];
 
+% init time
+time = time.long;
 
 %% create Hankel matrices
 
@@ -22,7 +24,7 @@ u = [u1; u2];
 Ly = size(y,1);
 Lu = size(u,1);
 s = 60;
-n = 6;
+n = 7;
 N = size(y,2) - 2*s + 1;
 
 % Hankel matrices
@@ -48,16 +50,14 @@ R22 = R(s*Lu+1:end, s*Lu+1:s*Lu+s*Ly);
 % R22 SVD
 [U, S, V] = svd(R22);
 Un = U(:,1:n);
-% figure
-% plot(diag(S),'ro','LineWidth',1);
-% set(gca,'YScale','log')
 
 % Determine Ct and At
 Ct = Un(1:Ly,:);
 At = Un(1:(s-1)*Ly,:)\Un(Ly+1:s*Ly,:);
 
 % Determine Bt and Dt
-% Phi = zeros(size(y,2),2*n+Lu);
+tic
+
 phi_1 = [];
 phi_2 = [];
 phi_3 = [];
@@ -93,14 +93,14 @@ xt0 = theta(1:n);
 Bt = reshape(theta(n+1:3*n),[n 2]);
 Dt = reshape(theta(3*n+1:end),[Ly,Lu]);
 
-
+toc
 %% Create the system
 sys = ss(At,Bt,Ct,Dt);
 G = tf(sys);
-xs = zeros(n,numel(time.block));
+xs = zeros(n,numel(time));
 
 xs(:,1) = xt0;
-for i = 1:numel(time.block)-1
+for i = 1:numel(time)-1
    xs(:,i+1) = At*xs(:,i) + Bt*u(:,i);
 end
 
@@ -111,24 +111,50 @@ SID.At = At;
 SID.Bt = Bt;
 SID.Ct = Ct;
 SID.Dt = Dt;
-SID.xt0 = xT0;
+SID.xt0 = xt0;
 
 save('..\..\Data\SID.mat','SID');
 %% Plotting
-% Update StepBlock figure
-figure(stepBlock)
-subplot(2,2,1)
+
+compSubID = figure('Name','SubID Comparison');
+
+subplot(2,1,1)
+xlabel('Time in s')
+ylabel('Temperature in /degC',  'Interpreter', 'Latex')
+title('Temperature Heater 1')
 hold on
-plot(time.block,ys(1,:),'k','LineWidth',2)
-% legend('y_{1,exp}','y_{1,subID}')
+plot(time,y1,'b.')
+plot(time,ys(1,:),'r','Linewidth',2)
 hold off
-subplot(2,2,2)
+legend({'y_{1,exp}','y_{1,subID}'},'Location','northeast')
+
+subplot(2,1,2)
+xlabel('Time in s')
+ylabel('Temperature in /degC',  'Interpreter', 'Latex')
+title('Temperature Heater 2')
 hold on
-plot(time.block,ys(2,:),'k','LineWidth',2)
-% legend('y_{2,exp}','y_{2,subID}')
+plot(time,y2,'b.')
+plot(time,ys(2,:),'r','Linewidth',2)
 hold off
+legend({'y_{2,exp}','y_{2,subID}'},'Location','northeast')
 
-% Singular Values
+sgtitle('Comperison experiment data and subspace identification output');
 
+% save figure
+compSubID = gcf;
+compSubID.Renderer = 'painters';
+saveas(compSubID, '..\..\Latex\images\SYSID\compSubID', 'svg');
 
+% Singular values
+singularVal = figure('Name','Singular Values Y0sN');
 
+xlabel('singular values')
+ylabel('Amplitude')
+title('Singular Values of Y0sN')
+plot(svd(Y0sN),'ro','LineWidth',1);
+set(gca,'YScale','log')
+
+% save figure
+singularVal = gcf;
+singularVal.Renderer = 'painters';
+saveas(singularVal, '..\..\Latex\images\SYSID\singularVal', 'svg');
